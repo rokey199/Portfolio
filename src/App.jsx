ee5f380325ce4e8d2936104f3bf29c5fbb523a91
Kishan Dabhi <78599876+rokey199@users.noreply.github.com>
2026-05-26 00:36:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
 
+import Navbar from './components/Navbar'
+import Hero from './sections/Hero'
+import About from './sections/About'
+import Skills from './sections/Skills'
+import Projects from './sections/Projects'
+import Contact from './sections/Contact'
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
+  )
+}
